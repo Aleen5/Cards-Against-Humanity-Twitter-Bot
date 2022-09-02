38ea3c91726b1ec99ea3d61ef4739e7735144d62
Aleen5 <rodriguezespinosaa@gmail.com>
2022-09-03 00:51:06 +0200

    Añadido control de si la carta blanca va justo al principio de la carta negra

--- combination.js
+++ combination.js
@@ -10,21 +10,29 @@ exports.combo = () => {
     let text = randomBlackCard.text
     let textR = ""
 
+    // Si la carta es una pregunta y no dispone de espacios en blanco, le añade un espacio en blanco al final
     if (!text.includes("_"))
         if (text.endsWith("?")) text += " _"
 
+    // Cambia los Insert Name por tags en twitter
     if (text.includes("Insert Name" || text.includes("Insert Name's")))
         text.replace("Insert Name" || "Insert Name's", "[tag a friend]")
 
     for (let i = 0; i < randomBlackCard.pick; i++) {
         randomWhiteCards.push(cards.white[pickRandomNumber(cards.white.length)])
         textR = `${randomWhiteCards[i]}`
+
+        // Capitaliza la carta blanca si la carta negra comienza con un espacio en blanco
+        if (text.startsWith("_")) {
+            textR.charAt(0) = textR.charAt(0).toUpperCase
+        }
+
         text = text.replace("_", textR)
     }
 
     if (text.endsWith(".") && text.charAt(text.length - 1) == ".") 
         text = text.substring(0, text.length - 1)
-        
+
     console.log(text)
     return text
 }
